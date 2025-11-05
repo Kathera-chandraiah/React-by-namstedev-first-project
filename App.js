bc33e09218dfb6637d9e29f05826fc4de606e5c4
chandu <chandukathera13@gmail.com>
2025-11-05 17:16:53 +0530

    mys second commit to my project

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "https://esm.sh/react@19.2.0";
-import ReactDOM from "https://esm.sh/react-dom@19.2.0/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", {}, "This heading from react!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
